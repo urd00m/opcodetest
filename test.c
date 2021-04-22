4ecb273ea2ccbef5fb7be3d68b1ca14f931ece0a
urd00m <alan.linghao.wang@gmail.com>
2021-04-22 09:26:17 -0500

    Log File work and some updates to C files

--- test.c
+++ test.c
@@ -4,9 +4,15 @@
 int oprun(void); 
 int testop(void);
 void first(void); 
+void second(void); 
+void breakop(void); 
+
 
 int main() {
+
+	//breakop(); //breaks the program (undefined opcode) 
 	first();
+	second(); 
 	int d = testop(); 
 	printf("%d\n", d);   
 
